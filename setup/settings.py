6978aeeecaf9c40aee9c04d420b5797aaffe0b90
Rafael Fernandes <rafaelrofernandes@gmail.com>
2024-10-03 11:14:33 -0300

    Adicionando settings

--- setup/settings.py
+++ setup/settings.py
@@ -27,18 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = True  
 
-ALLOWED_HOSTS = ['44.202.26.177']
+ALLOWED_HOSTS = []
 
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
 
-SECURE_SSL_REDIRECT = True
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
 
 
 # Application definition
